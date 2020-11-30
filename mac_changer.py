e166eb33bbc683e91b488cf728a4ca65b3f336f9
ODYTRON <otigas@hotmail.com>
2020-11-30 12:24:16 -0500

    added subprocess module and basic variables for interface and mac adress

---
+++ mac_changer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import subprocess
+
+interface = "eth0"
+new_mac = "00:11:22:33:44:77"
+
+print("[+] Changing MAC address for " + interface + "to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
