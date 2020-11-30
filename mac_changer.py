fa692981c8679e860505690601f3c2919e3f2e52
ODYTRON <otigas@hotmail.com>
2020-11-30 13:54:13 -0500

    Getting input from the user

--- mac_changer.py
+++ mac_changer.py
@@ -2,8 +2,9 @@
 
 import subprocess
 
-interface = "eth0"
-new_mac = "00:11:22:33:44:77"
+# for python 2 instead of 3 the function is raw_input instead of input
+interface = raw_input("interface > ")
+new_mac = raw_input("NEW MAC ADDRESS > ")
 
 print("[+] Changing MAC address for " + interface + "to " + new_mac)
 
