f499ac07be07c20a74bd8b14b0bc9afae8f5701d
ODYTRON <otigas@hotmail.com>
2020-12-01 08:31:11 -0500

    Handling input from the user using lists as a more secure way

--- mac_changer.py
+++ mac_changer.py
@@ -6,8 +6,24 @@ import subprocess
 interface = raw_input("interface > ")
 new_mac = raw_input("NEW MAC ADDRESS > ")
 
-print("[+] Changing MAC address for " + interface + "to " + new_mac)
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+
+# call subprocess module to use call command first way
+# seperated command calls
+
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+
+############################################################################################
+
+# call subprocess module to use call command second way
+# more secure way to handle inputs and execute commands by putting our commands in lists
+# by this way you have specific args that the program accepts
+# so it is difficult for somebody to intercept our code.
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
