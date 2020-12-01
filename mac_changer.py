a6a9a1f1003b1053947169153393ff54acfc7720
ODYTRON <otigas@hotmail.com>
2020-12-01 08:46:15 -0500

    HLet the user parse the program and the commands in terminal line at once with a parser

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+# use commands of the OS and terminal in programs
 import subprocess
+
+# Let the user parse the program and the commands in terminal line at once
+import optparse
+
+# parser object to handle user input
+parser = optparse.OptionParser()
+
+# this instance will handle the user input (-i or --interface to start)
+parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
 
 # for python 2 instead of 3 the function is raw_input instead of input
 interface = raw_input("interface > ")
