146b1cdeb36ce2254437a1fa94d99d29db962c9c
ODYTRON <otigas@hotmail.com>
2020-12-03 08:17:05 -0500

    added function to get current mac address

--- mac_changer_last.py
+++ mac_changer_last.py
@@ -30,25 +30,31 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    # capture the value of the interface with check_output
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    # search ifconfig_result to capture only mac address with regular expressions
+    # thanks to pythex.org
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    # check if the interface has a mac
+    if mac_address_search_result:
+        # print only the first result with group(0)
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
 # first we capture options and arguments with get arguments
 options = get_arguments()
+# get current mac
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 
 # then we call the function
 # change_mac(options.interface, options.new_mac)
 
-# capture the value of the interface with check_output
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
 
-# search ifconfig_result to capture only mac address with regular expressions
-# thanks to pythex.org
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
-# check if the interface has a mac
-if mac_address_search_result:
-    # print only the first result with group(0)
-    print(mac_address_search_result.group(0))
-else:
-    print("[-] Could not read MAC address")
 
 
