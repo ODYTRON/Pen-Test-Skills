4cf0d32db3fdac061fd81d5c222a5fef3f03d97e
ODYTRON <otigas@hotmail.com>
2020-12-08 14:02:01 -0500

    new files in the repo

---
+++ a_port_scanner.py
@@ -0,0 +1,50 @@
+#!/bin/python
+
+import sys
+import socket
+from datetime import datetime
+
+# Define our target
+
+if len(sys.argv) == 2:
+    # translate hostname to IPv4 the format of args is : python3 port_scanner.py <ip>
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("invalid amount of arguments")
+    print("format must be : python3 port_scanner.py <ip or HOST name>")
+
+# Add a banner to the program
+print("-" * 50)
+print("Scanning target" + target)
+print("Time started:" + str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(50, 85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))  # returns an error indicator
+        print("Checking port {}".format(port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\nExiting Program. ")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved. ")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server. ")
+    sys.exit()
+
+
+
+
+
+
+
+
--- network_scanner_v1.py
+++ network_scanner_v1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# goal : discover all clients in the same network
+
 # needs to be imported in settings in pycharm
 import scapy.all as scapy
 
@@ -12,14 +14,27 @@ def scan(ip):
 
     # create an ethernet frame (ethernet object) for the mac address communicaton (physical connection layer two)
     # you need to broadcast this mac address to all devices
-    # aka set destination MAC to broadcast MAC
+    # aka set destination MAC to broadcast MAC you can set specific MAC If you want
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     # print(broadcast.summary()) have the summary which sends a packet from my mac to all (broadcast)
     # then you have to combine two requests together
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
+    # print(arp_request_broadcast.summary())
     # with show() we see more details of a packet who sends what examples in last line
     # arp_request_broadcast.show()
+    # send and recieve function the response with values in two vars : answered and unanswered packets
+    # answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout = 1)
+    # print(answered_list.summary())
+    # print(unanswered_list.summary())
+    # capture only the  first element of the list (answered_list)
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    print(answered_list.summary())
+    # access elements of the list individually
+    print("IP\t\t\tMAC Address\n--------------------------------------------------")
+    for element in answered_list:
+        # the element has two subelements: the request sent and  the answer [0],[1]
+        # to print apropriate fields use .show()
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 
 
